d36e733d47e2a3be23b0da7b03d16bc7169e2ef9
Leo-Angelo1 <leoangelo175@gmail.com>
2025-08-20 04:44:27 -0300

    Project created

---
+++ src/test/java/com/leoangelo/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leoangelo.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
