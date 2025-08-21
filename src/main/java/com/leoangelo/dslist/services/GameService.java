1f31532eab7cf987a52da47675588d9f9e054b13
Leo-Angelo1 <leoangelo175@gmail.com>
2025-08-21 09:14:23 -0300

    End point get by id

--- src/main/java/com/leoangelo/dslist/services/GameService.java
+++ src/main/java/com/leoangelo/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.leoangelo.dslist.services;
 
+import com.leoangelo.dslist.DTO.GameDTO;
 import com.leoangelo.dslist.DTO.GameMinDTO;
 import com.leoangelo.dslist.entities.Game;
 import com.leoangelo.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +16,13 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
