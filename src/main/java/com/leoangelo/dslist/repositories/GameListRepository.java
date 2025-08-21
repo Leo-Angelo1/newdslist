182b5c98b5a8d507bb12d375aa086bd33f492fb4
Leo-Angelo1 <leoangelo175@gmail.com>
2025-08-21 10:15:32 -0300

    Create projection and end point lists/id/games

--- src/main/java/com/leoangelo/dslist/repositories/GameListRepository.java
+++ src/main/java/com/leoangelo/dslist/repositories/GameListRepository.java
@@ -2,7 +2,12 @@ package com.leoangelo.dslist.repositories;
 
 import com.leoangelo.dslist.entities.Game;
 import com.leoangelo.dslist.entities.GameList;
+import com.leoangelo.dslist.projection.GameMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface GameListRepository extends JpaRepository<GameList, Long> {
+
 }
--- src/main/java/com/leoangelo/dslist/services/GameService.java
+++ src/main/java/com/leoangelo/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.leoangelo.dslist.services;
 import com.leoangelo.dslist.DTO.GameDTO;
 import com.leoangelo.dslist.DTO.GameMinDTO;
 import com.leoangelo.dslist.entities.Game;
+import com.leoangelo.dslist.projection.GameMinProjection;
 import com.leoangelo.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,6 +28,12 @@ public class GameService {
         List<Game> result = gameRepository.findAll();
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
         return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 
 }
